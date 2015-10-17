c32dd6254dbe1aede42cf4a1feecf55b941409c2
jasonglisson <jason.glisson@gmail.com>
2015-10-17 16:18:31 -0400

    Edits to gcal

--- wp-content/themes/pittsafe/template-home-page.php
+++ wp-content/themes/pittsafe/template-home-page.php
@@ -49,6 +49,7 @@
 		</div>
 	    <div class="entry-content">
 		    <?php echo do_shortcode('[gcal id="123"]'); ?>
+		    <?php echo do_shortcode('[gcal id="247"]'); ?>		    
 <!-- 			<iframe src="http://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=400&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=pittsafe%40gmail.com&amp;color=%232952A3&amp;ctz=America%2FNew_York" style=" border-width:0 " width="100%" height="350" frameborder="0" scrolling="no"></iframe> -->
 	    </div>
 	</div>	
